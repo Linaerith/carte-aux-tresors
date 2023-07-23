d55d8c86201fed579d81586e634232c86617d8a8
Lindsay Grondin <lgrondin@steamulo.com>
2023-07-23 21:07:23 +0200

    feat: Export result of adventure in txt file

--- src/main/java/com/carbonit/carteauxtresors/CarteAuxTresorsApplication.java
+++ src/main/java/com/carbonit/carteauxtresors/CarteAuxTresorsApplication.java
@@ -1,72 +1,78 @@
 package com.carbonit.carteauxtresors;
 
 import com.carbonit.carteauxtresors.dto.AdventureDTO;
-import com.carbonit.carteauxtresors.entity.Adventurer;
-import com.carbonit.carteauxtresors.entity.Position;
-import com.carbonit.carteauxtresors.entity.Treasure;
-import com.carbonit.carteauxtresors.enums.Action;
-import com.carbonit.carteauxtresors.enums.Orientation;
 import com.carbonit.carteauxtresors.mapper.AdventureDTOMapper;
 import com.carbonit.carteauxtresors.service.ActionService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.ApplicationContext;
-import org.springframework.data.util.Pair;
-import org.springframework.util.StringUtils;
-import org.yaml.snakeyaml.util.EnumUtils;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.*;
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
-
-import static java.lang.Long.parseLong;
 
 @Slf4j
 @AllArgsConstructor
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class CarteAuxTresorsApplication {
 
-	public static void main(String[] args) {
-		ApplicationContext applicationContext = SpringApplication.run(CarteAuxTresorsApplication.class, args);
-		ActionService actionService = applicationContext.getBean(ActionService.class);
+    private static final String ABSOLUTE_PATH = "/home/lindsay/Documents/";
 
-		log.info("********************************************************");
-		log.info("*        Application carte aux trésors démarrée        *");
-		log.info("********************************************************");
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(CarteAuxTresorsApplication.class, args);
+        ActionService actionService = applicationContext.getBean(ActionService.class);
 
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please enter file path for the game initialization :");
-		String path = scanner.next();
-		AdventureDTO adventureDTO = AdventureDTO.builder().build();
-		File file = new File(path);
-		if(file.canRead()) {
-			try(BufferedReader br = new BufferedReader(new FileReader(file))) {
-				for(String line; (line = br.readLine()) != null; ) {
-					if (!line.trim().startsWith("#")) {
-						log.info("Processing line {}", line);
-						List<String> infos = List.of(line.split(" - "));
-						adventureDTO = AdventureDTOMapper.mapAdventureDTO(adventureDTO, infos);
-					}
+        log.info("********************************************************");
+        log.info("*        Application carte aux trésors démarrée        *");
+        log.info("********************************************************");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter file path for the game initialization :");
+        String path = scanner.next();
+        AdventureDTO adventureDTO = AdventureDTO.builder().build();
+        File file = new File(path);
+        if (file.canRead()) {
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                for (String line; (line = br.readLine()) != null; ) {
+                    if (!line.trim().startsWith("#")) {
+                        log.info("Processing line {}", line);
+                        List<String> infos = List.of(line.split(" - "));
+                        adventureDTO = AdventureDTOMapper.mapAdventureDTO(adventureDTO, infos);
+                    }
+                }
+            } catch (Exception e) {
+                log.error("Error while processing the file : {}", e.getMessage());
+                scanner.close();
+            }
+        }
+        scanner.close();
+
+        log.info("New adventure ready to begin {}", adventureDTO);
+        AdventureDTO finishedAdventure = actionService.handleAdventurersTurns(adventureDTO);
+
+        log.info("Adventure has finished {}", finishedAdventure);
+
+        String absoluteFilePath = String.format("%sAdventureResults-%s.txt", ABSOLUTE_PATH, LocalDateTime.now());
+
+		List<String> results = AdventureDTOMapper.mapAdventureDTOToString(finishedAdventure);
+        try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(absoluteFilePath, true));
+			results.forEach(result -> {
+				try {
+					writer.append(result);
+					writer.append("\n");
+				} catch (IOException e) {
+					throw new RuntimeException(e);
 				}
-			} catch (Exception e) {
-				log.error("Error while processing the file : {}", e.getMessage());
-				scanner.close();
-			}
-		}
-
-		log.info("New adventure ready to begin {}", adventureDTO);
-		AdventureDTO finishedAdventure = actionService.handleAdventurersTurns(adventureDTO);
-		log.info("Adventure has finished {}", finishedAdventure);
-	}
+			});
+			writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/test/java/com/carbonit/carteauxtresors/mapper/AdventureDTOMapperTest.java
+++ src/test/java/com/carbonit/carteauxtresors/mapper/AdventureDTOMapperTest.java
@@ -1,16 +1,45 @@
 package com.carbonit.carteauxtresors.mapper;
 
 import com.carbonit.carteauxtresors.dto.AdventureDTO;
+import com.carbonit.carteauxtresors.entity.Adventurer;
+import com.carbonit.carteauxtresors.entity.Position;
+import com.carbonit.carteauxtresors.entity.Treasure;
+import com.carbonit.carteauxtresors.enums.Action;
+import com.carbonit.carteauxtresors.enums.Orientation;
+import org.assertj.core.api.Assertions;
 import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AdventureDTOMapperTest {
+
+    @Test
+    public void given_ListStringCorrectlyFormattedWithUnknownObject_when_mapAdventureDTO_return_AdventureDTO() {
+        List<String> infos = Arrays.asList("R", "3", "4");
+        AdventureDTO adventureDTO = AdventureDTO.builder().build();
+        AdventureDTO updatedAdventureDTO = AdventureDTOMapper.mapAdventureDTO(adventureDTO, infos);
+
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getTreasures()).isNull();
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers()).isNull();
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getMountains()).isNull();
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getMapDimensions()).isNull();
+    }
+
+    @Test
+    public void given_ListStringCorrectlyFormattedMapDimensions_when_mapAdventureDTO_return_AdventureDTO() {
+        List<String> infos = Arrays.asList("C", "3", "4");
+        AdventureDTO adventureDTO = AdventureDTO.builder().build();
+        AdventureDTO updatedAdventureDTO = AdventureDTOMapper.mapAdventureDTO(adventureDTO, infos);
+
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getMapDimensions().getX()).isEqualTo(3L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getMapDimensions().getY()).isEqualTo(4L);
+    }
 
     @Test
     public void given_ListStringCorrectlyFormatted_when_mapMapDimensionsFromInfos_return_AdventureDTO() {
@@ -30,9 +59,233 @@ public class AdventureDTOMapperTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void given_ListStringNotCorrectlyFormatted_when_mapMapDimensionsFromInfos_return_AdventureDTO() {
+    public void given_ElementInListStringNotCorrectlyFormatted_when_mapMapDimensionsFromInfos_return_IllegalArgumentException() {
         List<String> infos = Arrays.asList("C", "test", "4");
         AdventureDTO adventureDTO = AdventureDTO.builder().build();
         AdventureDTOMapper.mapMapDimensionsFromInfos(adventureDTO, infos);
+    }
+
+    @Test
+    public void given_ListStringCorrectlyFormattedWithAdventurerInfos_when_mapAdventureDTO_return_AdventureDTO() {
+        List<String> infos = Arrays.asList("A", "Jim", "5", "4", "O", "ADA");
+        AdventureDTO adventureDTO = AdventureDTO.builder()
+                .mapDimensions(Position.builder().x(6L).y(5L).build())
+                .build();
+        AdventureDTO updatedAdventureDTO = AdventureDTOMapper.mapAdventureDTO(adventureDTO, infos);
+
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().size()).isEqualTo(1);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().get(0).getId()).isEqualTo(1L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().get(0).getName()).isEqualTo("Jim");
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().get(0).getActions().size()).isEqualTo(3);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().get(0).getPosition().getSecond()).isEqualTo(Orientation.O);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().get(0).getPosition().getFirst().getY()).isEqualTo(4L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().get(0).getPosition().getFirst().getX()).isEqualTo(5L);
+    }
+
+    @Test
+    public void given_ListStringCorrectlyFormatted_when_mapAdventurersFromInfos_return_AdventureDTO() {
+        List<String> infos = Arrays.asList("A", "Jim", "5", "4", "O", "ADA");
+        AdventureDTO adventureDTO = AdventureDTO.builder()
+                .mapDimensions(Position.builder().x(6L).y(5L).build())
+                .build();
+        AdventureDTO updatedAdventureDTO = AdventureDTOMapper.mapAdventurersFromInfos(adventureDTO, infos);
+
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().size()).isEqualTo(1);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().get(0).getId()).isEqualTo(1L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().get(0).getName()).isEqualTo("Jim");
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().get(0).getActions().size()).isEqualTo(3);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().get(0).getPosition().getSecond()).isEqualTo(Orientation.O);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().get(0).getPosition().getFirst().getY()).isEqualTo(4L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().get(0).getPosition().getFirst().getX()).isEqualTo(5L);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void given_ListStringCorrectlyFormattedButOutOfBound_when_mapAdventurersFromInfos_return_IllegalArgumentException() {
+        List<String> infos = Arrays.asList("A", "Jim", "6", "4", "O", "ADA");
+        AdventureDTO adventureDTO = AdventureDTO.builder()
+                .mapDimensions(Position.builder().x(6L).y(5L).build())
+                .build();
+        AdventureDTOMapper.mapAdventurersFromInfos(adventureDTO, infos);
+    }
+
+    @Test
+    public void given_ListStringCorrectlyFormattedWithAlreadyOneAdventurerInList_when_mapAdventurersFromInfos_return_AdventureDTO() {
+        List<String> infos = Arrays.asList("A", "Jim", "5", "4", "O", "ADA");
+        AdventureDTO adventureDTO = AdventureDTO.builder()
+                .mapDimensions(Position.builder().x(6L).y(5L).build())
+                .adventurers(Collections.singletonList(Adventurer.builder().id(1L).build()))
+                .build();
+        AdventureDTO updatedAdventureDTO = AdventureDTOMapper.mapAdventurersFromInfos(adventureDTO, infos);
+
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().size()).isEqualTo(2);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().get(0).getId()).isEqualTo(1L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().get(1).getId()).isEqualTo(2L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().get(1).getName()).isEqualTo("Jim");
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().get(1).getActions().size()).isEqualTo(3);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().get(1).getPosition().getSecond()).isEqualTo(Orientation.O);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().get(1).getPosition().getFirst().getY()).isEqualTo(4L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getAdventurers().get(1).getPosition().getFirst().getX()).isEqualTo(5L);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void given_ListStringNotCorrectlyFormatted_when_mapAdventurersFromInfos_return_IllegalArgumentException() {
+        List<String> infos = List.of("A", "Jim", "5", "4", "O");
+        AdventureDTO adventureDTO = AdventureDTO.builder().build();
+        AdventureDTOMapper.mapMapDimensionsFromInfos(adventureDTO, infos);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void given_ElementInListStringNotCorrectlyFormatted_when_mapAdventurersFromInfos_return_IllegalArgumentException() {
+        List<String> infos = Arrays.asList("A", "Jim", "5", "4", "O", "test");
+        AdventureDTO adventureDTO = AdventureDTO.builder().build();
+        AdventureDTOMapper.mapMapDimensionsFromInfos(adventureDTO, infos);
+    }
+
+    @Test
+    public void given_ListStringCorrectlyFormattedWithTreasureInfos_when_mapAdventureDTO_return_AdventureDTO() {
+        List<String> infos = Arrays.asList("T", "1", "2", "3");
+        AdventureDTO adventureDTO = AdventureDTO.builder()
+                .mapDimensions(Position.builder().x(6L).y(5L).build())
+                .build();
+        AdventureDTO updatedAdventureDTO = AdventureDTOMapper.mapAdventureDTO(adventureDTO, infos);
+
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getTreasures().size()).isEqualTo(1);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getTreasures().get(0).getNumber()).isEqualTo(3L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getTreasures().get(0).getPosition().getX()).isEqualTo(1L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getTreasures().get(0).getPosition().getY()).isEqualTo(2L);
+    }
+
+    @Test
+    public void given_ListStringCorrectlyFormatted_when_mapTreasuresFromInfos_return_AdventureDTO() {
+        List<String> infos = Arrays.asList("T", "1", "2", "3");
+        AdventureDTO adventureDTO = AdventureDTO.builder()
+                .mapDimensions(Position.builder().x(6L).y(5L).build())
+                .build();
+        AdventureDTO updatedAdventureDTO = AdventureDTOMapper.mapTreasuresFromInfos(adventureDTO, infos);
+
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getTreasures().size()).isEqualTo(1);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getTreasures().get(0).getNumber()).isEqualTo(3L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getTreasures().get(0).getPosition().getX()).isEqualTo(1L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getTreasures().get(0).getPosition().getY()).isEqualTo(2L);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void given_ListStringCorrectlyFormattedButOutOfBound_when_mapTreasuresFromInfos_return_IllegalArgumentException() {
+        List<String> infos = Arrays.asList("T", "3", "5", "3");
+        AdventureDTO adventureDTO = AdventureDTO.builder()
+                .mapDimensions(Position.builder().x(6L).y(5L).build())
+                .build();
+        AdventureDTOMapper.mapTreasuresFromInfos(adventureDTO, infos);
+    }
+
+    @Test
+    public void given_ListStringCorrectlyFormattedWithAlreadyOneTreasureInList_when_mapTreasuresFromInfos_return_AdventureDTO() {
+        List<String> infos = Arrays.asList("T", "1", "2", "3");
+        AdventureDTO adventureDTO = AdventureDTO.builder()
+                .mapDimensions(Position.builder().x(6L).y(5L).build())
+                .treasures(Collections.singletonList(Treasure.builder().number(4L).build()))
+                .build();
+        AdventureDTO updatedAdventureDTO = AdventureDTOMapper.mapTreasuresFromInfos(adventureDTO, infos);
+
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getTreasures().size()).isEqualTo(2);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getTreasures().get(0).getNumber()).isEqualTo(4L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getTreasures().get(1).getNumber()).isEqualTo(3L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getTreasures().get(1).getPosition().getX()).isEqualTo(1L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getTreasures().get(1).getPosition().getY()).isEqualTo(2L);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void given_ListStringNotCorrectlyFormatted_when_mapTreasuresFromInfos_return_IllegalArgumentException() {
+        List<String> infos = List.of("A", "Jim", "5", "4", "O");
+        AdventureDTO adventureDTO = AdventureDTO.builder().build();
+        AdventureDTOMapper.mapTreasuresFromInfos(adventureDTO, infos);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void given_ElementInListStringNotCorrectlyFormatted_when_mapTreasuresFromInfos_return_IllegalArgumentException() {
+        List<String> infos = Arrays.asList("T", "Jim", "1", "1");
+        AdventureDTO adventureDTO = AdventureDTO.builder().build();
+        AdventureDTOMapper.mapMapDimensionsFromInfos(adventureDTO, infos);
+    }
+
+    @Test
+    public void given_ListStringCorrectlyFormattedWithMountainInfos_when_mapAdventureDTO_return_AdventureDTO() {
+        List<String> infos = Arrays.asList("M", "1", "2");
+        AdventureDTO adventureDTO = AdventureDTO.builder()
+                .mapDimensions(Position.builder().x(6L).y(5L).build())
+                .build();
+        AdventureDTO updatedAdventureDTO = AdventureDTOMapper.mapAdventureDTO(adventureDTO, infos);
+
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getMountains().size()).isEqualTo(1);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getMountains().get(0).getX()).isEqualTo(1L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getMountains().get(0).getY()).isEqualTo(2L);
+    }
+
+    @Test
+    public void given_ListStringCorrectlyFormatted_when_mapMountainsFromInfos_return_AdventureDTO() {
+        List<String> infos = Arrays.asList("M", "1", "2");
+        AdventureDTO adventureDTO = AdventureDTO.builder()
+                .mapDimensions(Position.builder().x(6L).y(5L).build())
+                .build();
+        AdventureDTO updatedAdventureDTO = AdventureDTOMapper.mapMountainFromInfos(adventureDTO, infos);
+
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getMountains().size()).isEqualTo(1);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getMountains().get(0).getX()).isEqualTo(1L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getMountains().get(0).getY()).isEqualTo(2L);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void given_ListStringCorrectlyFormattedButOutOfBound_when_mapMountainFromInfos_return_IllegalArgumentException() {
+        List<String> infos = Arrays.asList("M", "3", "5");
+        AdventureDTO adventureDTO = AdventureDTO.builder()
+                .mapDimensions(Position.builder().x(6L).y(5L).build())
+                .build();
+        AdventureDTOMapper.mapMountainFromInfos(adventureDTO, infos);
+    }
+
+    @Test
+    public void given_ListStringCorrectlyFormattedWithAlreadyOneMountainInList_when_mapMountainFromInfos_return_AdventureDTO() {
+        List<String> infos = Arrays.asList("M", "1", "2");
+        AdventureDTO adventureDTO = AdventureDTO.builder()
+                .mapDimensions(Position.builder().x(6L).y(5L).build())
+                .mountains(Collections.singletonList(Position.builder().x(4L).y(4L).build()))
+                .build();
+        AdventureDTO updatedAdventureDTO = AdventureDTOMapper.mapMountainFromInfos(adventureDTO, infos);
+
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getMountains().size()).isEqualTo(2);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getMountains().get(0).getX()).isEqualTo(4L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getMountains().get(0).getY()).isEqualTo(4L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getMountains().get(1).getX()).isEqualTo(1L);
+        AssertionsForClassTypes.assertThat(updatedAdventureDTO.getMountains().get(1).getY()).isEqualTo(2L);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void given_ListStringNotCorrectlyFormatted_when_mapMountainFromInfos_return_IllegalArgumentException() {
+        List<String> infos = List.of("A", "Jim", "5", "4", "O");
+        AdventureDTO adventureDTO = AdventureDTO.builder().build();
+        AdventureDTOMapper.mapTreasuresFromInfos(adventureDTO, infos);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void given_ElementInListStringNotCorrectlyFormatted_when_mapMountainFromInfos_return_IllegalArgumentException() {
+        List<String> infos = Arrays.asList("M", "Jim", "1");
+        AdventureDTO adventureDTO = AdventureDTO.builder().build();
+        AdventureDTOMapper.mapMountainFromInfos(adventureDTO, infos);
+    }
+
+    @Test
+    public void given_StringOfActionsCorrectlyFormatted_when_mapStringToListOfActions_return_ListOfActions() {
+        String actions = "ADG";
+        List<Action> newActions = AdventureDTOMapper.mapStringToListOfActions(actions);
+        Assertions.assertThat(newActions).hasSize(3);
+        Assertions.assertThat(newActions.get(0)).isEqualTo(Action.A);
+        Assertions.assertThat(newActions.get(1)).isEqualTo(Action.D);
+        Assertions.assertThat(newActions.get(2)).isEqualTo(Action.G);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void given_StringOfActionsNotCorrectlyFormatted_when_mapStringToListOfActions_return_IllegalArgumentException() {
+        String actions = "AtestG";
+        AdventureDTOMapper.mapStringToListOfActions(actions);
     }
 }
